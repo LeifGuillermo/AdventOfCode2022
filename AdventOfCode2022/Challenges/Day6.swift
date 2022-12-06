32df488e2325b0b84932d2a036c6f70462f5f6f0
Leif Guillermo <guillermoleif@gmail.com>
2022-12-05 22:53:43 -0700

    Solve Day 6

--- AdventOfCode2022/Challenges/Day6.swift
+++ AdventOfCode2022/Challenges/Day6.swift
@@ -10,12 +10,32 @@ import Foundation
 class Day6: DayProtocol {
     var sampleInput: String = "day6sample.txt"
     var dayInput: String = "day6input.txt"
-    
+
     func solvePart1(_ input: [Substring]) {
-        print("Day6 part 1")
+        solvePart(input, numDistinctChars: 4)
     }
     
     func solvePart2(_ input: [Substring]) {
-        print("Day6 part 2")
+        solvePart(input, numDistinctChars: 14)
+    }
+    
+    func solvePart(_ input: [Substring], numDistinctChars: Int) {
+        let line: Substring = input[0]
+        for (idx, _) in input[0].enumerated() {
+            let isUnique = areNextFourCharactersUnique(currentIndex: idx, line: line, distinctChars: numDistinctChars)
+            if(isUnique) {
+                print("Index: ", idx + numDistinctChars)
+                return
+            }
+        }
+    }
+    
+    func areNextFourCharactersUnique(currentIndex: Int, line: Substring, distinctChars: Int = 4) -> Bool{
+        let start = line.index(line.startIndex, offsetBy: currentIndex)
+        let end = line.index(line.startIndex, offsetBy: currentIndex+distinctChars)
+        let range = start..<end
+        let nextFour = line[range]
+        let charSet = Set(nextFour)
+        return charSet.count == distinctChars
     }
 }
--- AdventOfCode2022/main.swift
+++ AdventOfCode2022/main.swift
@@ -12,4 +12,5 @@ import Foundation
 //Day3().solveDay()
 //Day4().solveDay()
 //Day5().solveDay()
+//Day6().solveDay()
 Day6().solveDay()
